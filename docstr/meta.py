abcf8ef862caa6198f2b41cad2222fae82d99440
prijatelj <theomnipotentwoof@gmail.com>
2021-04-08 16:27:03 -0400

    added todos for checks if docs match args/kwargs & basic func code.

--- docstr/meta.py
+++ docstr/meta.py
@@ -3,3 +3,7 @@
 # TODO decorator for filling in args and kwargs from the docstring. Write once.
 
 # TODO decorator for optional type checking funcitonality
+
+# TODO function to generate docs from current args and kwargs in given style
+# TODO decorator to check the docs to match current args and kwargs.
+#   TODO optionally, correcting them w/ overriding, but defaults to alert user.
