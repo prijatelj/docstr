b0e6a6f1f3cf1d75a8f4d7a1240939f943bb4f51
prijatelj <theomnipotentwoof@gmail.com>
2022-03-06 18:53:36 -0500

    style

--- tests/docstr_checking.py
+++ tests/docstr_checking.py
@@ -1,5 +1,6 @@
 """Tests of the compiler-like checking of docstrings to ensure they parse
 styles as they are expected and includes examples of out-of-style
-docstrings."""
+docstrings.
+"""
 
 # TODO
