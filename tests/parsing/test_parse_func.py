faa9db73a9b24212c8dac437337042edc01de570
prijatelj <theomnipotentwoof@gmail.com>
2022-03-08 17:03:32 -0500

    added fixtures for func tests

--- tests/parsing/test_parse_func.py
+++ tests/parsing/test_parse_func.py
@@ -11,42 +11,67 @@ from docstr import docstring, parse
 
 import testing.numpy_example_docstrings as examples
 
-# TODO given tests/numpy_example_docstrings.py, parse the docstrings and check
-# the resulting parsed docstring objects.
 
-# TODO pytest hook to setup the common dependencies behind these tests. Write
-# once, after all...
-
-
-class TestParseFunc:
-    @pytest.mark.dependency(name='TestParseFunc')
-    def test_docstr_parse_func():
-        # Create the expected result of docstring.
-        expected = docstring.FuncDocstring(
-            examples.numpy_doc_func.__name__,
-            FunctionType,
-            'This is the short desc. of the function, concat the paired strings',
-            args=OrderedDict(
-                foo=docstring.ArgDoc(
-                    'foo',
-                    str,
-                    'Foo is an excellently documented string argument.',
-                ),
-                bar=docstring.ArgDoc(
-                    'bar',
-                    str,
-                    'Bar is an excellently documented string argument.',
-                )
-            ),
-            return_doc=BaseDoc(
-                'Returns',
+@pytest.fixture
+def expected_func():
+    return docstring.FuncDocstring(
+        examples.numpy_doc_func.__name__,
+        FunctionType,
+        'This is the short desc. of the function, concat the paired strings',
+        args=OrderedDict(
+            foo=docstring.ArgDoc(
+                'foo',
                 str,
-                'An incredible ordered concatenation of the paired string inputs.',
+                'Foo is an excellently documented string argument.',
+            ),
+            bar=docstring.ArgDoc(
+                'bar',
+                str,
+                'Bar is an excellently documented string argument.',
             )
+        ),
+        return_doc=BaseDoc(
+            'Returns',
+            str,
+            'An incredible ordered concatenation of the paired string inputs.',
         )
+    )
 
-        # Decide if parser is function or class object.
-        parsed = parse(examples.numpy_doc_func)
 
-        # Comparison supported betweend the docstr.docstring classes.
+@pytest.fixture
+def expected_func_defaults(expected_func):
+    expected_func.args['foo'].default = 'foo'
+    expected_func.args['bar'].default = 'bar'
+    return expected_func
+
+
+@pytest.fixture
+def expected_func_choices(expected_func_defaults):
+    choices = docstring.MultiType({'foo', 'bar'})
+    expected_func_defaults.args['foo'].choices = choices
+    expected_func_defaults.args['bar'].choices = choices
+    return expected_func_defaults
+
+
+@pytest.mark.dependency(name='TestParseFunc')
+class TestParseFunc:
+    @pytest.mark.dependency(name='TestParseFunc.func')
+    def test_docstr_parse_func(expected_func):
+        parsed = parse('numpy', examples.numpy_doc_func)
         assert expected == parsed
+
+    @pytest.mark.dependency(
+        name='TestParseFunc.defaults',
+        defaults=['TestParseFunc.func'],
+    )
+    def test_docstr_parse_func_defaults(expected_func_defaults):
+        parsed = parse('numpy', examples.numpy_doc_func_defaults)
+        assert expected_func_defaults == parsed
+
+    @pytest.mark.dependency(
+        name='TestParseFunc.choices',
+        defaults=['TestParseFunc.defaults'],
+    )
+    def test_docstr_parse_func_choices(expected_func_choices):
+        parsed = parse('numpy', examples.numpy_doc_func_choices)
+        assert expected_func_choices == parsed
