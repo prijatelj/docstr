38e007397f12fd34e0c33b023425fd6713facb43
prijatelj <theomnipotentwoof@gmail.com>
2022-03-08 17:32:56 -0500

    added fixture extention and updated test.

--- tests/parsing/test_parse_class_linking.py
+++ tests/parsing/test_parse_class_linking.py
@@ -1,6 +1,5 @@
 """Tests of the compiler-like checking of docstrings to ensure they parse
-styles as they are expected and includes examples of out-of-style
-docstrings.
+styles as they are expected and includes examples of out-of-style docstrings.
 """
 from collections import OrderedDict
 from types import FunctionType
@@ -10,8 +9,37 @@ import pytest
 from docstr import docstring, parse
 
 import testing.numpy_example_docstrings as examples
-from testing.parsing.test_parse_class_with_primitives import \
-    TestParseClassPrimitives
+from testing.parsing.test_parse_class_with_primitives import (
+    TestParseClassPrimitives,
+    expected_class_of_primitives,
+)
+
+
+@pytest.fixture
+def expected_class_linking(expected_class_of_primitives):
+    expected_class_of_primitives.attributes.update(
+        example_var=docstring.ArgDoc('example_var', object),
+        is_this_correct=docstring.ArgDoc(
+            'is_this_correct',
+            bool,
+            default=False,
+        ),
+    )
+    expected_class_of_primitives.methods.update(
+        bar=FuncDocstring(
+            'bar',
+            FunctionType,
+            'Same args as foo, but performs a different operation.',
+            args=expected_class_of_primitives.methods['foo'].args
+        )
+    )
+    return docstring.ClassDocstring(
+        examples.NumpyDocClassLinkning.__name__,
+        examples.NumpyDocClassLinkning,
+        'A Numpy example class that uses docstring linking.',
+        attributes=expected_class_of_primitives.attributes,
+        methods=expected_class_of_primitives.methods,
+    )
 
 
 class TestParseClassLinking(TestParseClassPrimitives):
@@ -19,12 +47,6 @@ class TestParseClassLinking(TestParseClassPrimitives):
         name='TestParseClassLinking',
         depends=['TestParseClassPrimitives'],
     )
-    def test_class_doc_linking():
-        # TODO use the given pre-made hook expected item.
-        # TODO Create the expected result of docstring.
-
-        # Decide if parser is function or class object.
-        parsed = parse(examples.NumpyDocClassLinking)
-
-        # Comparison supported betweend the docstr.docstring classes.
-        assert expected == parsed
+    def test_class_doc_linking(expected_class_linking):
+        parsed = parse('numpy', examples.NumpyDocClassLinking)
+        assert expected_class_linking == parsed
