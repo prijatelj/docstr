9a4f8709500543d27cc005a755ba39fbf6818fe5
prijatelj <theomnipotentwoof@gmail.com>
2022-03-08 17:37:26 -0500

    WIP: adding fixture for multi linking.

--- tests/parsing/test_parse_class_multi_linking.py
+++ tests/parsing/test_parse_class_multi_linking.py
@@ -10,8 +10,38 @@ import pytest
 from docstr import docstring, parse
 
 import testing.numpy_example_docstrings as examples
-from testing.parsing.test_parse_class_with_primitives import \
-    TestParseClassLinking
+from testing.parsing.test_parse_class_with_primitives import (
+    TestParseClassLinking,
+    expected_class_linking,
+)
+
+
+@pytest.fixture
+def expected_class_multi_linking(expected_class_linking):
+    expected_class_linking
+    return docstring.ClassDocstring(
+        examples.NumpyDocClassMultiLinkning.__name__,
+        examples.NumpyDocClassMultiLinkning,
+        """A Numpy example class that uses multiple docstring linking of depth 2
+        links.
+
+        An example of linking text from the long description of another docstring:
+        DO QUOTING MARKDOWNFOR SPHINX:
+            $see `NumpyDocLinkingClass`$
+
+        An example of linking a subset of text from the long description of another
+        docstring:
+        DO QUOTING MARKDOWNFOR SPHINX:
+            # TODO make this the Math LaTex and hyperlinking
+            $see `NumpyDocClass`[3:5]$
+
+        # TODO labeled/subsection linking to avoid using indexing of long
+        # description by lines?
+        """,
+        attributes=expected_class_of_primitives.attributes,
+        methods=expected_class_of_primitives.methods,
+    )
+
 
 
 class TestParseClassMultiLinking(TestParseClassLinking):
@@ -19,11 +49,8 @@ class TestParseClassMultiLinking(TestParseClassLinking):
         name='TestParseClassMultiLinking',
         depends=['TestParseClassLinking'],
     )
-    def test_class_doc_2hop_linking_and_non_primitive_arg():
-        # TODO Create the expected result of docstring.
-
-        # Decide if parser is function or class object.
+    def test_class_doc_2hop_linking_and_non_primitive_arg(
+        expected_class_multi_linking
+    ):
         parsed = parse(examples.NumpyDocClassMultiLinking)
-
-        # Comparison supported betweend the docstr.docstring classes.
         assert expected == parsed
