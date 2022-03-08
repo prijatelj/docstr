97c4c23c1e87ea97d090ca639b99f7a356e3e619
prijatelj <theomnipotentwoof@gmail.com>
2022-03-08 15:32:31 -0500

    rm unnecessary test, as this is handled by sphinx.

--- tests/style_conversions.py
+++
@@ -1,5 +0,0 @@
-"""This file contains the unit tests docstring invertible style conversions."""
-
-
-# TODO Test Custom DocString parsing and conversions:
-# 1 test: go from Numpy example to custom and back.
