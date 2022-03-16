f78bccac73909323adb142cc4ebc63277aa4ef29
prijatelj <theomnipotentwoof@gmail.com>
2022-03-15 20:28:51 -0400

    added todos on config generation from given object's docstrings and hierarchical parsing of yaml -- at least -- as a config file type.

--- tests/configargparse/yaml_pipeline_config_parsing.py
+++ tests/configargparse/yaml_pipeline_config_parsing.py
@@ -1,5 +1,9 @@
 """Tests of the YAML config parser with hierarchy and pipelining."""
 
-# TODO Given an example case, create a yaml config w/ hierarchy and generate
-# the configargparser for that yaml file then test it to see if it has all the
-# attributes it should w/ the correct values.
+# TODO Hierarchical namespaces in ConfigArgParse
+# given an example yaml config, see if it is correctly parsed by a
+# configargparser with hierarchial namespaces.
+
+# Empty and Default ConfigArgParse yaml config generation via parsed docstrings
+# TODO Given the numpy example functions
+# TODO Given the numpy example classes
