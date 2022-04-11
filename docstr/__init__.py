ddb5d2243363ae347ed0693f624e22ce7b981cea
prijatelj <theomnipotentwoof@gmail.com>
2022-04-11 02:28:31 -0400

    added more of docstr files into init

--- docstr/__init__.py
+++ docstr/__init__.py
@@ -1,2 +1,16 @@
-__version__ = '0.0.1'
-from .parsing import parse
+from importlib import import_module
+__version__='0.0.1'
+
+__all__ = [
+    'cli',
+    'configargparse',
+    'docstring',
+    'parsing',
+]
+
+for module in __all__:
+    globals()[module] = import_module(f'.{module}', __name__)
+del import_module, module
+
+__all__ += ['parse', 'parse_config']
+from .parsing import parse, parse_config
